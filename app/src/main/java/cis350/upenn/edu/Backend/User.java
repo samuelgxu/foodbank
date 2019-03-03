6f00821623e3352b19a266af6df6aa7e89dc76c4
samuelxu <samuelgxu@gmail.com>
2019-03-02 19:25:34 -0500

    updated style

--- app/src/main/java/cis350/upenn/edu/Backend/User.java
+++ app/src/main/java/cis350/upenn/edu/Backend/User.java
@@ -4,6 +4,7 @@ public class User {
     private static int userIDCounter = 0;
     private int UserID;
     private String username;
+    private String email;
     private String password;
 
     public User(String username, String password){
@@ -12,5 +13,27 @@ public class User {
         this.password = password;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
